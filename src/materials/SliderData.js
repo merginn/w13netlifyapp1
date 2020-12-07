8770a6f2186c2f24c876a3306a5e9639e5861a95
Murathan Ergin <murathan.egin@gmail.com>
2020-12-07 12:40:13 +0200

    Initial commit

---
+++ src/materials/SliderData.js
@@ -0,0 +1,14 @@
+export const SliderData = [
+    {
+        image: 'https://avatars0.githubusercontent.com/u/1?v=4'
+    },
+    {
+        image: 'https://avatars0.githubusercontent.com/u/5?v=4'
+    },
+    {
+        image: 'https://avatars0.githubusercontent.com/u/7?v=4'
+    },
+    {
+        image: 'https://avatars0.githubusercontent.com/u/6?v=4'
+    }
+];
